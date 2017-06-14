eac4f87162cb4b2bb50319c6a52186c1ef09706c
Mykola Rudnev <RudnevMykola@gmail.com>
2017-06-14 14:38:40 +0300

    Created MainActivity and layout

--- app/src/main/java/com/example/mykolarudnev/remindme/MainActivity.java
+++ app/src/main/java/com/example/mykolarudnev/remindme/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.mykolarudnev.remindme;
 
-import android.app.Activity;
+
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -10,13 +11,14 @@ import android.view.MenuItem;
  * Created by Николай on 03.06.2017.
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
    private Toolbar toolbar;
 
 
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -29,12 +31,11 @@ public class MainActivity extends Activity {
         toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
+            public boolean onMenuItemClick(MenuItem menuItem) {
                 return false;
             }
         });
 
         toolbar.inflateMenu(R.menu.menu);
-
     }
 }
