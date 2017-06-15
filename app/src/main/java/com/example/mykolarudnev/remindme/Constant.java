9c8bf253663c84af3f8dfda176f1c3c4a0cfc514
Mykola Rudnev <RudnevMykola@gmail.com>
2017-06-15 19:12:08 +0300

    Implements navigation view and  tabs navigation with test  fragment ,

---
+++ app/src/main/java/com/example/mykolarudnev/remindme/Constant.java
@@ -0,0 +1,12 @@
+package com.example.mykolarudnev.remindme;
+
+/**
+ * Created by Николай on 15.06.2017.
+ */
+
+public class Constant {
+
+    public static final  int TAB_ONE=0;
+    public static final  int TAB_TWO=1;
+    public static final  int TAB_THREE=2;
+}
